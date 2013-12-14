59e62238380d8d09023048a34010c6f03963ceb4
danilko09 <kolesnikov.da0@gmail.com>
2013-12-14 16:40:34 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php 
 class Config{
-    const db_pref = "develop_";
+    const db_pref = "test_";
     const db_host = "localhost";
     const db_user = "root";
     const db_pass = "";
     const db_name = "test";
-    const site_url = "http://my.local/cms";
-    const site_name = "CMS Develop Site";
+    const site_url = "";
+    const site_name = "";
 }
 ?>
