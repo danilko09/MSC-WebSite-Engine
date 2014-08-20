eed1bec5c39b00197e754274d55d4d4759128eb8
Данил <danilko@danil-nout.pawel-home>
2014-08-20 12:47:10 +0400

    add: db_setup module

--- cms/config.php
+++ cms/config.php
@@ -4,7 +4,7 @@ class Config{
 	
 
 	//site
-	const site_url = "http://dev.local";
+	const site_url = "http://msc-wse.esy.es";
 	const site_name = "dev_local site";
 	const def_template = "green";
 	const def_module = "pages";
