10bf33577ea8490c510e1f93f9e971ad7707312e
danilko09 <kolesnikov.da0@gmail.com>
2015-01-17 22:21:14 +0400

    Чет как-то лаганул git

--- setup/scripts/finish.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$ech = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-	$ech = str_replace("/setup", "", $ech);
-	$content = "Установка завершена.<br/>Все необходимые расширениябыли установлены автоматичесски.<br/><br/><a href='$ech'>На сайт</a> | <a href='".$ech."index.php/admin'>В административную панель.</a>";
-	//Далее должно быть удаление директории setup, но пока просто сброс утановки в начало
-	$_SESSION = Array();
-	
-	$finish = true;
